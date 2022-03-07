1e10d4cef6ff16d6088e0d1e43c3ab9ceca3e097
Ignacio Senra <isenras@hotmail.com>
2022-03-07 01:16:27 -0300

    paginas con parciales hbs

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-const express = require('express');
+const express = require("express");
+const hbs = require("hbs");
+
 const app = express();
 const port = 8080;
 
+//HBS
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
 //Middlewares
 // Para servidor contenido estatico
-app.set('view engine', 'hbs');
 
-app.use(express.static('public'));
-
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hello World perro');
+app.use(express.static("public"));
+app.get("/hola-mundo", (req, res) => {
+  res.send("Hello World perro");
 });
 
-app.get('/', (req, res) => {
-  res.render('home', {
-    nombre: 'Ignacio Senra',
-    titulo: 'Curso node',
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Ignacio Senra",
+    titulo: "Curso node",
   });
 });
 
-app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html');
+app.get("/generic", (req, res) => {
+  res.render("generic", {
+    nombre: "Ignacio Senra",
+    titulo: "Curso node",
+  });
 });
 
-app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html');
+app.get("/elements", (req, res) => {
+  res.render("elements", {
+    nombre: "Ignacio Senra",
+    titulo: "Curso node",
+  });
 });
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/404.html');
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(port, () => {
