ec11c4765428be6ae62093980d4990860aa231ee
Ignacio Senra <isenras@hotmail.com>
2022-03-08 12:05:12 -0300

    Despliegue app angular

--- app.js
+++ app.js
@@ -1,45 +1,16 @@
 const express = require("express");
-const hbs = require("hbs");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
-//HBS
-app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
-
 //Middlewares
 // Para servidor contenido estatico
 
 app.use(express.static("public"));
-app.get("/hola-mundo", (req, res) => {
-  res.send("Hello World perro");
-});
-
-app.get("/", (req, res) => {
-  res.render("home", {
-    nombre: "Ignacio Senra",
-    titulo: "Curso node",
-  });
-});
-
-app.get("/generic", (req, res) => {
-  res.render("generic", {
-    nombre: "Ignacio Senra",
-    titulo: "Curso node",
-  });
-});
-
-app.get("/elements", (req, res) => {
-  res.render("elements", {
-    nombre: "Ignacio Senra",
-    titulo: "Curso node",
-  });
-});
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
